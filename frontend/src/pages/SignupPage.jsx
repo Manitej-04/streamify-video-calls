a1940490a72f7f16bbd4103ebfd0332b76b9cdb3
Manitej <151040895+Manitej-04@users.noreply.github.com>
2025-07-18 20:17:29 +0530

    Update SignupPage.jsx

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { ShipWheelIcon } from "lucide-react";
 import { Link } from 'react-router';
 
-import useSignUp from '../hooks/useSignup.js';
+import useSignUp from '../hooks/useSignUp';
 
 const SignupPage = () => {
   const [signupData, setSignupData] = useState({
